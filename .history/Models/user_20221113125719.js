ed1ffba8276f5d4ff881240b9791d4579fe19bc6
Divyanshu <divyanshukalola88@gmail.com>
2022-11-13 13:37:14 -0600

    user model update

---
+++ .history/Models/user_20221113125719.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const apiKey = new mongoose.Schema({
+  key: "String",
+  date: { type: Date, default: Date.now },
+});
+
+const userSchema = new mongoose.Schema({
+  _id: {
+    type: String,
+    required: true,
+    default: mongoose.Types.ObjectId,
+  },
+  apiKeys: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
---
+++ .history/Models/user_20221113130605.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const apiKey = new mongoose.Schema({
+  key: { type: String },
+  date: { type: Date, default: Date.now },
+});
+
+const appointmentDF = new mongoose.Schema({
+  slotSize: { type: Number, required: true },
+});
+
+const userSchema = new mongoose.Schema({
+  _id: {
+    type: String,
+    required: true,
+    default: mongoose.Types.ObjectId,
+  },
+  apiKeys: [apiKey],
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  phone: { type: String, required: true },
+  date: {
+    type: Date,
+    default: Date.now,
+    required: true,
+  },
+  subStartDate: {
+    type: Date,
+  },
+  subEndDate: {
+    type: Date,
+  },
+  appointmentDF: {
+    type: userSchema,
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
