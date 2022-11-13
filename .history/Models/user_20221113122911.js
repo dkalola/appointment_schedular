8e1309790407147408e01cbf95ed13c4d143b750
Divyanshu <divyanshukalola88@gmail.com>
2022-11-13 12:49:13 -0600

    post update

---
+++ .history/Models/user_20221113122911.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const appointmentSchema = new mongoose.Schema({
+  _id: {
+    type: String,
+    required: true,
+    default: mongoose.Types.ObjectId,
+  },
+  guestID: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Appointment", appointmentSchema);
---
+++ .history/Models/user_20221113123134.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  _id: {
+    type: String,
+    required: true,
+    default: mongoose.Types.ObjectId,
+  },
+  apiKeys: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+    required: true,
+  },
+  location: {d
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
