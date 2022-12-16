29c7ece7e17fc2405db77f45d3bd69aad9161873
Divyanshu <divyanshukalola88@gmail.com>
2022-12-15 21:31:20 -0600

    update

---
+++ .history/app_20221215171806.js
@@ -0,0 +1,78 @@
+const express = require("express");
+var path = require("path");
+const app = express();
+const router = express.Router();
+
+// firebase
+
+var admin = require("firebase-admin");
+
+// middleware
+router.use(express.json());
+router.use(express.urlencoded({ extended: false }));
+
+app.set("view engine", "ejs");
+app.use("/static", express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+app.get("/test2", (req, res) => {
+  res.sendFile(__dirname + "/views/test2.html");
+});
+
+app.get("/date_select", (req, res) => {
+
+  if (req.query.apiKey) {
+    res.render("date_select", {
+      type: req.query.type ? req.query.type : 1,
+      btnc: req.query.type == 0 ? req.query.btnc : "E0E0E0",
+      color: req.query.type == 0 ? req.query.color : "FFFFFF",
+      apiKey: req.query.apiKey,
+      br: req.query.br ? req.query.br : 10,
+    });
+  } else {
+    res.status(404).send("API Key Not Found");
+  }
+});
+
+app.get("/docs", (req, res) => {
+  res.sendFile(__dirname + "/views/docs.html");
+});
+
+app.get("/pricing", (req, res) => {
+  res.sendFile(__dirname + "/views/pricing.html");
+});
+
+app.get("/blog", (req, res) => {
+  res.sendFile(__dirname + "/views/blog.html");
+});
+
+app.get("/blog-single", (req, res) => {
+  res.sendFile(__dirname + "/views/blog-single.html");
+});
+
+app.get("/contact", (req, res) => {
+  res.sendFile(__dirname + "/views/contact.html");
+});
+
+app.get("/user", (req, res) => {
+  res.sendFile(__dirname + "/views/user_account.html");
+});
+
+app.post("/register", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await admin.auth().createUser({
+      email,
+      password,
+    });
+    res.send(user);
+  } catch (e) {
+    res.send(e);
+  }
+});
+
+
+module.exports = app;
